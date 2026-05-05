3a560cc833724c8530bcd27275624810fd137613
nik-rocha <nicollasrocha320@gmail.com>
2026-05-04 22:22:05 -0300

    :sparkles: 04/05/2026

--- exerciciosFila.py
+++ exerciciosFila.py
@@ -1,4 +1,47 @@
-## Prática Rápida
+# ## Prática Rápida
+
+# class Queue:
+    
+#     def __init__(self):
+#         self.elements = []
+        
+#     def isEmpty(self):
+#         return self.elements == []
+    
+#     def enqueue(self, item):
+#         self.elements.insert(0, item)
+        
+#     def dequeue(self):
+#         return self.elements.pop()
+    
+#     def size(self):
+#         return len(self.elements)
+    
+#     def show(self):
+#         for i, element in enumerate(self.elements):
+#             if i == 0:
+#                 print(f"INÍCIO -> {element}", end="")
+#             elif i == (len(self.elements) - 1):
+#                 print(f" -> {element} <- FIM\n", end="")
+#             else:
+#                 print(f" -> {element}", end="")
+                
+# fila = Queue()
+# fila.enqueue(100)
+# fila.enqueue(200)
+# fila.enqueue(300)
+# fila.enqueue(400)
+# fila.show()
+
+# fila.dequeue()
+# fila.dequeue()
+# fila.show()
+
+# fila.enqueue(500)
+# fila.enqueue(600)
+# fila.show()
+
+# 1. Atendimento na Recepção
 
 class Queue:
     
@@ -20,23 +63,22 @@ class Queue:
     def show(self):
         for i, element in enumerate(self.elements):
             if i == 0:
-                print(f"INÍCIO -> {element}", end="")
+                print(f"{element}, ", end="")
             elif i == (len(self.elements) - 1):
-                print(f" -> {element} <- FIM\n", end="")
+                print(f"{element}\n", end="")
             else:
-                print(f" -> {element}", end="")
+                print(f"{element}, ", end="")
                 
 fila = Queue()
-fila.enqueue(100)
-fila.enqueue(200)
-fila.enqueue(300)
-fila.enqueue(400)
+fila.enqueue("Ana")
+fila.enqueue("Bruno")
+fila.enqueue("Carlos")
+fila.enqueue("Diana")
+print(f"Entrada: ", end="")
 fila.show()
 
-fila.dequeue()
-fila.dequeue()
+print(f"Saída: {fila.dequeue()}, {fila.dequeue()}")
 fila.show()
 
-fila.enqueue(500)
-fila.enqueue(600)
+print(f"Entrada: ", end="")
 fila.show()
